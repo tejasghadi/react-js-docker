b0b2c9d0fbd637a8432011c416e1dda3c7d1e44d
root <root@ip-172-31-21-6.us-west-2.compute.internal>
2019-07-22 14:58:12 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Hi there
+          Bye there
+          I was changed
         </p>
         <a
           className="App-link"
